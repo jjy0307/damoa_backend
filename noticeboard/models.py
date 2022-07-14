1e2252fef5155d81d655a50060fdf12dc3cae5e5
Seungtae Lee <90381057+SingToLive@users.noreply.github.com>
2022-07-14 13:23:38 +0900

    ADD noticeboard/models.py
    noticeboard app을 생성하면서 만들어졌습니다
    아직 커스텀 admin은 안되어있는 상태입니다

---
+++ noticeboard/models.py
@@ -0,0 +1,17 @@
+from django.db import models
+from community.models import Community as CommunityModel
+from user.models import CustomUser as CustomUserModel
+
+# Create your models here.
+class Noticeboard(models.Model):
+    community = models.ForeignKey(CommunityModel, on_delete=models.CASCADE)
+    name = models.CharField(max_length=20)
+    is_public = models.BooleanField(default=True)
+    created_date = models.DateTimeField(auto_now_add=True)
+    modified_date = models.DateTimeField(auto_now = True)
+    
+class UserAndNoticeboard(models.Model):
+    user = models.ForeignKey(CustomUserModel, on_delete=models.CASCADE)
+    noticeboard = models.ForeignKey(Noticeboard, on_delete=models.CASCADE)
+    is_creator = models.BooleanField(default=False)
+    date_joined = models.DateTimeField(auto_now_add=True)
