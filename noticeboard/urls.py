0feabf0be05b2e9c046a2ae2592e2dae38274cd7
soiyo <soiyosauce@gmail.com>
2022-07-19 15:47:10 +0900

    MOD noticeboard/urls.py UserAndNoticeboard 클래스 볼수있게 수정

--- noticeboard/urls.py
+++ noticeboard/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path, include
-from .views import NoticeboardViewSet
+from .views import NoticeboardViewSet, UserAndNoticeboardViewSet
 
 # 게시판 보기, 생성
 noticeboard_create = NoticeboardViewSet.as_view({
@@ -7,9 +7,15 @@ noticeboard_create = NoticeboardViewSet.as_view({
     'post':'create'
 })
 
+#UserAndNoticeboard 클래스 보기
+noticeboard_create_list = UserAndNoticeboardViewSet.as_view({
+    'get':'list',
+})
+
 
 urlpatterns = [
     path('noticeboard/create/', noticeboard_create),
+    path('noticeboard/create/list', noticeboard_create_list),
 ]
 
 
