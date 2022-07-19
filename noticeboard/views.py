262a931fecc41907ab5a6a483c4f195fcb75dfe5
soiyo <soiyosauce@gmail.com>
2022-07-19 15:47:22 +0900

    MOD noticeboard/views.py UserAndNoticeboard 클래스 볼수있게 수정

--- noticeboard/views.py
+++ noticeboard/views.py
@@ -1,7 +1,11 @@
-from .models import Noticeboard
-from .serializers import NoticeboardSerializer
+from .models import Noticeboard, UserAndNoticeboard
+from .serializers import NoticeboardSerializer, UserAndNoticeboardSerializer
 from rest_framework import viewsets
 
 class NoticeboardViewSet(viewsets.ModelViewSet):
     queryset = Noticeboard.objects.all()
     serializer_class = NoticeboardSerializer
+    
+class UserAndNoticeboardViewSet(viewsets.ModelViewSet):
+    queryset = UserAndNoticeboard.objects.all()
+    serializer_class = UserAndNoticeboardSerializer
