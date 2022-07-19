525a7566cd8cf64566c2c4c03a87a3d833d13ea7
soiyo <soiyosauce@gmail.com>
2022-07-19 15:47:01 +0900

    MOD noticeboard/serializers.py UserAndNoticeboard 클래스 볼수있게 수정

--- noticeboard/serializers.py
+++ noticeboard/serializers.py
@@ -1,7 +1,13 @@
-from .models import Noticeboard
+from .models import Noticeboard, UserAndNoticeboard
 from rest_framework import serializers
 
 class NoticeboardSerializer(serializers.ModelSerializer):
     class Meta:
         model = Noticeboard
-        fields = ['name']
+        fields = ['community', 'name', 'is_public', 'created_date', 'modified_date']
+        
+class UserAndNoticeboardSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = UserAndNoticeboard
+        fields = ['user', 'noticeboard', 'is_creator', 'date_joined']
+        
