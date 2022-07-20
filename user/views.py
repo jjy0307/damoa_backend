37047f55b458d4a6c93d4ede9d7b455ea8584b49
Seungtae Lee <90381057+SingToLive@users.noreply.github.com>
2022-07-20 17:19:27 +0900

    MOD user/views.py
    회원가입 로그인 추가

--- user/views.py
+++ user/views.py
@@ -1,3 +1,30 @@
 from django.shortcuts import render
-
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from user.serializers import CustomUserSerializer, CustomUserTokenObtainPairSerializer
+from rest_framework import status, permissions
+from django.contrib.auth import login, authenticate
+from rest_framework_simplejwt.views import TokenObtainPairView
+from rest_framework_simplejwt.authentication import JWTAuthentication
 # Create your views here.
+
+class UserView(APIView):
+    def post(self, request):
+        user_serializer = CustomUserSerializer(data=request.data)
+        if user_serializer.is_valid():
+            user_serializer.save()
+            return Response({"message": "가입 완료"}, status=status.HTTP_200_OK)
+        return Response(user_serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
+class JwtTokenObtainPairView(TokenObtainPairView):
+    serializer_class = CustomUserTokenObtainPairSerializer
+    
+class OnlyAuthenticatedUserView(APIView):
+    permission_classes = [permissions.IsAuthenticated]
+    authentication_classes = [JWTAuthentication]
+
+    def get(self, request):
+        user = request.user
+        if not user:
+            return Response({"error": "접근 권한이 없습니다."}, status=status.HTTP_401_UNAUTHORIZED)
+        return Response({"message": "인증 성공!"})
