d08031f770588f74966ea919e7b183a730ca0692
soiyo <soiyosauce@gmail.com>
2022-07-22 12:27:37 +0900

    MOD article urls.py 필요요한 데이터 가져오느라 수정

--- article/urls.py
+++ article/urls.py
@@ -4,6 +4,7 @@ from . import views
 
 urlpatterns = [
     path("article/", views.ArticleList.as_view()),
+    path("article/user/", views.ArticleUserList.as_view()),
     path("article/write/", views.ArticleAdd.as_view()),
     path("article/<int:pk>/write/", views.ArticleDetail.as_view()),
     path("article/<int:pk>/write/put", views.ArticleMod.as_view()),
