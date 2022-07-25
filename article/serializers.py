becd8f366ceb2fc689aca300dc4cdd672704d70d
soiyo <99478108+soiyo@users.noreply.github.com>
2022-07-25 18:17:29 +0900

    MOD serializer.py

--- article/serializers.py
+++ article/serializers.py
@@ -3,18 +3,14 @@ from rest_framework import serializers
 
 from rest_framework.exceptions import ValidationError
 
+
 class ArticleSerializer(serializers.ModelSerializer):
-<<<<<<< HEAD
-    print("gggggg")
-    class Meta:         
-=======
     user_name = serializers.SerializerMethodField()
 
     def get_user_name(self, obj):
         return obj.user.username
 
     class Meta:
->>>>>>> 570d79a2236c0e3616af0bc4525e1ed72828b28c
         model = Article
         fields = [
             "id",
@@ -28,32 +24,34 @@ class ArticleSerializer(serializers.ModelSerializer):
             "image",
             "file",
         ]
+
     def validate(self, data):
-        if not data['noticeboard']:
-            print('noticebaord is suck', data['noticeboard'])
-            raise ValidationError('noticeboard error')
+        if not data["noticeboard"]:
+            print("noticebaord is suck", data["noticeboard"])
+            raise ValidationError("noticeboard error")
         # if not data['user']:
         #     print('user is suck', data['user'])
         #     raise ValidationError('user error')
-        if not data['title']:
-            print('title is suck', data['title'])
-            raise ValidationError('title error')
-        if not data['content']:
-            print('content is suck', data['content'])
-            raise ValidationError('content error')
+        if not data["title"]:
+            print("title is suck", data["title"])
+            raise ValidationError("title error")
+        if not data["content"]:
+            print("content is suck", data["content"])
+            raise ValidationError("content error")
         # if not data['created_date']:
         #     print('created_date is suck', data['created_date'])
         #     raise ValidationError('created_date error')
         # if not data['modified_date']:
         #     print('modified_date is suck', data['modified_date'])
         #     raise ValidationError('modified_date error')
-        if not data['image']:
-            print('image is suck', data['image'])
-            raise ValidationError('image error')
+        if not data["image"]:
+            print("image is suck", data["image"])
+            raise ValidationError("image error")
         # if not data['file']:
         #     print('file is suck', data['file'])
         #     raise ValidationError('file error')
         return data
+
 
 class ArticleLikesSerializer(serializers.ModelSerializer):
     class Meta:
