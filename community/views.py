4fcad44653ad5c26027b7fbd3d9436bb8796088a
Seungtae Lee <90381057+SingToLive@users.noreply.github.com>
2022-07-26 00:21:05 +0900

    MOD community/views.py
    아직 실험중
    request.user까지는 완료

--- community/views.py
+++ community/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
-
+from rest_framework.views import APIView
+from rest_framework.response import Response
 # Create your views here.
+class MainLoginedCommunity(APIView):
+    def get(self, request):
+        print('request', request)
+        print('request.user', request.user)
+        return Response({'message':'sexypower'})
