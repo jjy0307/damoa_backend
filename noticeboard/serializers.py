6ac9b2b849df20269c4abd268512aee662df370a
soiyo <soiyosauce@gmail.com>
2022-08-01 08:33:38 +0900

    MOD 깃풀시 충돌해결

--- noticeboard/serializers.py
+++ noticeboard/serializers.py
@@ -2,13 +2,14 @@ from .models import Noticeboard, UserAndNoticeboard
 from rest_framework import serializers
 from article.serializers import ArticleSerializerForNoticeboard
 
+
 class NoticeboardSerializer(serializers.ModelSerializer):
     # article_set = ArticleSerializerForNoticeboard(many=True, read_only=True)
 
     article_set = serializers.SerializerMethodField()
 
     def get_article_set(self, instance):
-        articles = instance.article_set.all().order_by('-created_date')
+        articles = instance.article_set.all().order_by("-created_date")
         return ArticleSerializerForNoticeboard(articles, many=True).data
 
     # article_info = serializers.SerializerMethodField(read_only=True)
