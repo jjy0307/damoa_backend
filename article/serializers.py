d35aa6cb917660c582e5334395f33c12067d4540
Seungtae Lee <90381057+SingToLive@users.noreply.github.com>
2022-08-03 00:31:38 +0900

    MOD article/serializers.py
    ArticleToolSerializer를 통해 아티클과 이미지 저장, 이미지는 한장씩 for문을 통해 저장

--- article/serializers.py
+++ article/serializers.py
@@ -1,3 +1,4 @@
+from asyncore import read
 from .models import Article, ArticleLikes, Comment, CommentLikes, ArticleAndImage
 from rest_framework import serializers
 from rest_framework.exceptions import ValidationError
@@ -142,3 +143,22 @@ class ArticleSerializerForNoticeboard(serializers.ModelSerializer):
             "modified_date",
             "file",
         ]
+        
+class ArticleAndImageToolSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = ArticleAndImage
+        fields = ['article', 'image']
+        
+class ArticleToolSerializer(serializers.ModelSerializer):
+    images = ArticleAndImageToolSerializer(many=True, read_only=True)
+    class Meta:
+        model = Article
+        fields = ['images', 'noticeboard', 'user', 'title', 'content', 'file', 'is_valid']
+    def create(self, validate_data):
+        instance = Article.objects.create(**validate_data)
+        image = self.context
+        for image_data in image:
+            ArticleAndImage.objects.create(article=instance, image=image[image_data])
+        return instance
+
+        
