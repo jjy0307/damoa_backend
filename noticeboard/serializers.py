32f35ba8d6bb9229bf3d34411f91463bfceda16c
soiyo <soiyosauce@gmail.com>
2022-08-03 18:08:45 +0900

    MOD noticeboard/serializers.py NoticeboardSerializer 에 get_community_name 함수 추가하여 community_name가져옴

--- noticeboard/serializers.py
+++ noticeboard/serializers.py
@@ -5,16 +5,21 @@ from article.serializers import ArticleSerializerForNoticeboard
 
 class NoticeboardSerializer(serializers.ModelSerializer):
     article_set = serializers.SerializerMethodField()
+    community_name = serializers.SerializerMethodField()
 
     def get_article_set(self, instance):
         articles = instance.article_set.all().order_by("-created_date")
         return ArticleSerializerForNoticeboard(articles, many=True).data
+
+    def get_community_name(self, obj):
+        return obj.community.name
 
     class Meta:
         model = Noticeboard
         fields = [
             "id",
             "community",
+            "community_name",
             "name",
             "is_public",
             "created_date",
