148991e47eabe22735f89b58d8fafd9fcb27510b
Seungtae Lee <90381057+SingToLive@users.noreply.github.com>
2022-08-03 12:22:53 +0900

    MOD noticeboard/urls.py
    새로운 경로 설정
    게시판 하니씩 받는 경로 설정

--- noticeboard/urls.py
+++ noticeboard/urls.py
@@ -5,6 +5,7 @@ from . import views
 urlpatterns = [
     path("create/", views.NoticeboardList.as_view()),
     path("create/<int:pk>/", views.NoticeboardDetail.as_view()),
+    path("view/<int:pk>/", views.NoticeboardObject.as_view()),
 ]
 
 urlpatterns = format_suffix_patterns(urlpatterns)
