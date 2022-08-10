161ccb3c7d3fbcb804c037bcec55d2781b64fa12
Seungtae Lee <90381057+SingToLive@users.noreply.github.com>
2022-08-11 02:53:05 +0900

    MOD community/urls.py
    해당 위치의 url이 아니므로 삭제 user로 이동

--- community/urls.py
+++ community/urls.py
@@ -6,6 +6,5 @@ urlpatterns = [
     path("main/", views.MainRecommendationCommunity.as_view()),
     path("main/my_recommendation/", views.MainLoginedRecommendationCommunity.as_view()),
     path("main/my_community/", views.MainLoginedCommunity.as_view()),
-    path("main/mypage/", views.MypageForCommunity.as_view()),
     path("main/create", views.MainCreateCommunity.as_view()),
 ]
