50c2676e8cbe4339c12cf920ad3ad9576295a4fd
Seungtae Lee <90381057+SingToLive@users.noreply.github.com>
2022-08-10 17:42:21 +0900

    MOD community/urls.py
    url 세분화
    main - 로그인 안된 상태에서 첫화면
    main/my_recommendation - 로그인 상태에서 가입 되지 않은 커뮤니티
    main/my_community - 로그인 상테에서 가입된 커뮤니티

--- community/urls.py
+++ community/urls.py
@@ -3,7 +3,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("main/", views.MainLoginedCommunity.as_view()),
+    path("main/", views.MainRecommendationCommunity.as_view()),
+    path("main/my_recommendation/", views.MainLoginedRecommendationCommunity.as_view()),
+    path("main/my_community/", views.MainLoginedCommunity.as_view()),
     path("main/mypage/", views.MypageForCommunity.as_view()),
     path("main/create", views.MainCreateCommunity.as_view()),
 ]
